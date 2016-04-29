0ce3b1067b1fd643b26975af9316d6391f8f424c
Heather Tovey <heather@htovey.com>
2016-04-29 15:26:19 -0600

    Update TwitchStreamers look and functionality.

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 var t,
 TwitchList = {
 	settings: {
-		twitchList: document.getElementById("twitch-list"),
+		twitchOnline: document.getElementById("twitch-online"),
+		twitchOffline: document.getElementById("twitch-offline"),
+		showAll: document.getElementById("show-all"),
+		showOnline: document.getElementById("show-online"),
+		showOffline: document.getElementById("show-offline"),
+		onlineList: document.getElementById("online-list"),
+		offlineList: document.getElementById("offline-list"),
 		twitchUsers: {
 			'freecodecamp': {},
 			'geekandsundry': {},
@@ -19,10 +25,29 @@ TwitchList = {
 		t = TwitchList.settings;
 		TwitchList.bindUIActions();
 		TwitchList.createList(t.twitchUsers);
+
+
+
+		t.animationEvent = TwitchList.whichAnimationEvent();
+		t.onlineList.addEventListener(t.animationEvent, TwitchList.removeFading);
+
+		t.animationEvent = TwitchList.whichAnimationEvent();
+		t.offlineList.addEventListener(t.animationEvent, TwitchList.removeFading);
 	},
 
 	bindUIActions: function() {
 
+		t.showAll.addEventListener("click", function() {
+			TwitchList.showAll();
+		})
+
+		t.showOnline.onclick = function() {
+			TwitchList.showOnline(t.showOnline);
+		};
+
+		t.showOffline.onclick = function() {
+			TwitchList.showOffline(t.showOffline);
+		};
 	},
 
 	createList: function(userList) {
@@ -113,12 +138,100 @@ TwitchList = {
 	},
 
 	appendData: function(user) {
-
 		var item = document.createElement('li');
-		item.innerHTML = "<div><img class='user-img' src=" + user.logo + "><h2><a href=" + user.url + ">" + user.display_name + "</h2></a><p class='status'>" + user.status + "</p></div>";
-		item.className += "twitch-user";
-		t.twitchList.appendChild(item);
+		item.innerHTML = "<a class='status-link' href=" + user.url + "><div class='user-container waves-light waves-effect btn'><img class='user-img' src=" + user.logo + "><h2 class='username'>" + user.display_name + "</h2><p class='status'>" + user.status + "</p></div></a>";
+		item.className += "twitch-user animated fadeInUp";
+		
 
+		if (user.status === "Not currently streaming...") {
+			t.twitchOffline.insertBefore(item, t.twitchOffline.firstChild);
+		} else if (user.status === "This username does not exist.") {
+			t.twitchOffline.appendChild(item);
+		} else {
+			t.twitchOnline.appendChild(item);
+		}
+	},
+
+	addClass: function(element, addedClass) {
+		if (element.classList) {
+		  element.classList.add(addedClass);
+		}
+		else {
+		  element.className += ' ' + addedClass;
+		}
+	},
+
+	removeClass: function(element, removedClass) {
+		if (element.classList) {
+		  element.classList.remove(removedClass);
+		}
+		else {
+		  element.className = element.className.replace(new RegExp('(^|\\b)' + removedClass.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+		}
+	},
+
+	showAll: function() {
+
+		TwitchList.addClass(t.onlineList, "fadeInUp");
+		TwitchList.addClass(t.offlineList, "fadeInUp");
+
+		TwitchList.removeClass(t.onlineList, "hide");
+		TwitchList.removeClass(t.offlineList, "hide");
+
+		t.animationEvent = TwitchList.whichAnimationEvent();
+		t.onlineList.addEventListener(t.animationEvent, TwitchList.removeFading);
+
+		t.animationEvent = TwitchList.whichAnimationEvent();
+		t.offlineList.addEventListener(t.animationEvent, TwitchList.removeFading);
+		
+	},
+
+	showOnline: function() {
+		TwitchList.addClass(t.onlineList, "fadeInUp");
+
+		TwitchList.removeClass(t.offlineList, "fadeInUp");
+
+		TwitchList.removeClass(t.onlineList, "hide");
+		TwitchList.addClass(t.offlineList, "hide");
+
+		t.animationEvent = TwitchList.whichAnimationEvent();
+		t.onlineList.addEventListener(t.animationEvent, TwitchList.removeFading);
+	},
+
+	showOffline: function() {
+		TwitchList.addClass(t.offlineList, "fadeInUp");
+
+		TwitchList.removeClass(t.onlineList, "fadeInUp");
+
+		TwitchList.removeClass(t.offlineList, "hide");
+		TwitchList.addClass(t.onlineList, "hide");
+
+		t.animationEvent = TwitchList.whichAnimationEvent();
+		t.offlineList.addEventListener(t.animationEvent, TwitchList.removeFading);
+
+	},
+
+	whichAnimationEvent: function() {
+	    var t;
+	    var el = document.createElement('fakeelement');
+	    var animations = {
+	      'animation':'animationend',
+	      'OAnimation':'oAnimationEnd',
+	      'MozAnimation':'animationend',
+	      'WebkitAnimation':'webkitAnimationEnd'
+	    }
+
+	    for(t in animations){
+	        if( el.style[t] !== undefined ){
+	            return animations[t];
+	        }
+	    }
+
+	},
+
+	removeFading: function() {
+		this.removeEventListener(t.animationEvent, TwitchList.removeFading);
+		TwitchList.removeClass(this, "fadeInUp");
 	}
 
 
